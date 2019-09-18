a93a0f5ba9d3a881aaa87afc3cf3e1eab316cd9a
Rui Li <lirui.newlife@gmail.com>
2019-09-18 10:53:33 +1000

    Button: "Start", "Stop", "Lap", "Reset" all work.

--- main.js
+++ main.js
@@ -1,70 +1,100 @@
-$(document).ready(function(){})
-
-var trigger = false;
+var isRunning = false;
 function triggerStopwatch(){
-    if(trigger==false){
+    if(isRunning==false){
         startStopwatch();
-        trigger = true;
+        isRunning = true;
     }else{
-        stopwatchRun();
+        stopStopwatch();
+        isRunning = false;
     }
 }
 
 // What to do after clicking 'Start' button.
 var stopwatchInterval;
-var m = '0'+'0';
-var s = '0'+'0';
-var ms = '0'+'0';
+var m = 0;
+var s = 0;
+var ms = 0;
+var btnText = $('#lap').text();;
 function startStopwatch(){
-    $('#realTime').text(m+':'+s+'.'+ms);
     $('#start').text('Stop');
     $('#start').addClass('stop');
     $('#lap').removeAttr('disabled');
     stopwatchInterval = setInterval(stopwatchRun,10);
+    $('#lap').text('Lap');
 }
 
 // Stopwatch starts working.
-var i=0;
 function stopwatchRun(){
-    if(i<10){
-        ms = '0' + i;
-    }else if(i<100){
-        ms = i;
-    }else if(i==100 && s<10){
-        ms = '0'+'0';
-        s = '0' + s + 1;
-    }else if(s==60 && m<10){
-        s = '0'+'0';
-        m = '0' + m + 1;
-    }else if(m<60){
+    ms += 1;
+    if (ms==100){
+        ms = 0;
+        s += 1;
+    }
+    if(s==60){
+        s = 0;
         m += 1;
     }
-    i++;
+    
+    var realTime = '';
+    if (ms<10) {
+        realTime += '0'+ ms;
+    } else {
+        realTime += ms;
+    }
+    realTime = '.' + realTime;
+
+    if (s<10) {
+        realTime = '0'+ s + realTime;
+    } else {
+        realTime = s + realTime;
+    }
+    realTime = ':' + realTime;
+
+    if (m<10) {
+        realTime = '0'+ m + realTime;
+    } else {
+        realTime = m + realTime;
+    }
+    $('#realTime').text(realTime);
+
 }
 
 // What to do after clicking 'Stop' button.
 function stopStopwatch(){
     $('#start').text('Start');
-    $('#start').removeClass('btn btn-secondary').addClass('btn btn-dark');
+    $('#start').removeClass('stop');
     $('#lap').text('Reset');
     clearInterval(stopwatchInterval);
 }
 
 // What to do after clicking 'Lap' button.
-function recordTime(){
-     $('#laps').append(lapTime);
-     var lapTime = '<tr>'+ title + time + '</tr>';
+var num = 1;
+function recordTime(){ 
      var title = '<td>Lap'+ num +'</td>'; 
-     var time = '<td>'+ currentTime +'</td>';
      var currentTime = $('#realTime').text();
+     var time = '<td>'+ currentTime +'</td>';
+     var lapTime = '<tr>'+ title + time + '</tr>';   
+     num += 1;
+     $('#laps').append(lapTime);
+     btnText = $('#lap').text();
+     if (btnText == 'Reset') {
+        resetStopwatch();
+     }
+
 }
 
 // What to do after clicking 'Reset' button.
 function resetStopwatch(){
-     m = '0'+'0';
-     s = '0'+'0';
-     ms = '0'+'0';
-     $('#realTime').text(m+':'+s+'.'+ms);
+     m = 0;
+     s = 0;
+     ms = 0;
+     var realTime = '0'+m+':0'+s+'.0'+ms;
+     $('#realTime').text(realTime);
      $('#laps tr').remove();
+     $('#lap').text('lap');
+     $( "#lap" ).prop( "disabled", true );
 }
+
+
+
 
